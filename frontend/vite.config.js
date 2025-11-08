d9650595e351e0387f0c267b1b47a3eb7892fe5d
unknown <paviyarajaganapathy@gmail.com>
2025-11-08 09:59:26 +0530

    Update vite config for github deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Set correct base path for GitHub Pages hosting
 export default defineConfig({
   plugins: [react()],
-  base: '/voice-AI-assistant/', // <-- 👈 must match your GitHub repo name
+  base: '/voice-AI-assistant/', // this must match GitHub repo name
 })
+
